edbdcda6d6ec30636c94fd316c6f97bb787732bb
Izaelle Lavigne <izaelleavrillavigne@gmail.com>
2024-07-26 18:28:19 -0300

    feat: initial

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+export const Header = () => {
+  return (
+    <header className="p-4 flex items-center justify-between">
+      <img src="logo.svg" alt="logo" className="svg" />
+      <div className="flex items-center gap-6">
+        <div className="flex items-center gap-4">
+          <a className="app-text" href="">Sobre Nós</a>
+          <a className="app-text" href="">Acessibilidade</a>
+        </div>
+        <div className="flex items-center gap-4">
+          <button>
+            <img src="profile.svg" alt="" />
+          </button>
+          <button>
+            <img src="favorite.svg" alt="" />
+          </button>
+          <button>
+            <img src="bag.svg" alt="" />
+          </button>
+        </div>
+      </div>
+    </header>
+  )
+ 
+}

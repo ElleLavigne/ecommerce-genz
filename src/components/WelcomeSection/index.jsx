a75c42b643eb6eb2bd4ccf51e9649ba59c1b241e
Izaelle Lavigne <izaelleavrillavigne@gmail.com>
2024-07-27 10:26:42 -0300

    feat: welcome animations

--- src/components/WelcomeSection/index.jsx
+++ src/components/WelcomeSection/index.jsx
@@ -15,26 +15,19 @@ export const WelcomeSection = () => {
           </div>
         </Container>
       </div>
-      <div className="pt-[154px]  h-[90vh] relative">
-        {/* <img
-          className="-z-10 inset-0 rounded-[50px] absolute w-full h-full object-cover"
-          src="background.png"
-          alt=""
-        /> */}
-        <Image
+      <div className="pt-[154px]  h-[90vh] relative app-welcome-background">
+        {/* <Image
           fill
-          className="-z-10 inset-0 rounded-[50px] absolute w-full h-full object-cover"
+          className="-z-10 inset-0 rounded-[50px] absolute w-full h-full object-cover app-welcome-image"
           src="/background.png"
           alt=""
-        />
+        /> */}
         <Container>
-          {/* <span className="">We believe in</span> */}
           <h1 className="app-fade text-[94px] text-white">We believe in</h1>
-
-          <h1 className="app-fade-2 text-[94px] text-white">free fashion</h1>
-          {/* <span>free fashion</span> */}
-
-          <h2 className="text-[32px] text-white max-w-[668px]">
+          <h1 className="app-fade app-delay-500 text-[94px] text-white">
+            free fashion
+          </h1>
+          <h2 className="text-[32px] text-white max-w-[668px] app-fade app-delay-1000">
             Fashion is an art of expression, and we take it very seriously.
           </h2>
         </Container>
